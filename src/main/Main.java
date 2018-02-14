e8fcdfd9ebfcfaaec8e8e7e05c33b26e6890aef0
liamjg <gallagherl0491@my.uwstout.edu>
2018-02-13 22:31:58 -0600

    Added comments

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,29 +12,40 @@ public class Main {
 	
 	public static void main(String args[]) throws IOException {
 		
+		//open file containing questions
 		File file = new File("test.txt");
 		
+		//parse the file of questions into objects
 		Vector<Question> questions = parseQuestions(file);
 		
+		//start the user test
 		startTest(questions);
 	}
 	
+	//user test
 	public static void startTest(Vector<Question> questions) {
 		
 		Vector<Question> shuffQuestions = questions;
+		//randomize the order of the questions
 		Collections.shuffle(shuffQuestions);
 		
+		//create scanner to get users answer
 		Scanner scanner = new Scanner(System.in);
 		
+		//create variable to hold the score
 		int score = 0;
 		
-		
+		//for loop to iterate through the questions
 		for(int i = 0; i < shuffQuestions.size(); i++) {
 			
+			//get the current question
 			Question currQuestion = shuffQuestions.elementAt(i);
 			
+			//print the current question
 			System.out.println(currQuestion.getQuestion());
 			
+			//check user's input against the answer
+			//if the answer is correct print a message and increment the score
 			if(currQuestion.checkAnswer(scanner.nextLine())) {
 				System.out.println("Correct!" + '\n');
 				score++;
@@ -44,6 +55,7 @@ public class Main {
 			
 			String scorePrint = "Score: " + score + "/" + shuffQuestions.size() + '\n';
 
+			//print the current score. Give the user a message based on how many answers are correct
 			if(i == shuffQuestions.size()-1) {
 				System.out.println("Final " + scorePrint);
 				if(score >= shuffQuestions.size()*0.6) {
@@ -54,17 +66,18 @@ public class Main {
 			}else{
 				System.out.println(scorePrint);
 			}
-
-			
 		}
 		
 		scanner.close();
 		
 	}
 	
+	//question parsing method
 	public static Vector<Question> parseQuestions(File file) throws IOException {
+		//create vector to hold question objects
 		Vector<Question> questions = new Vector<Question>();
 		
+		//parse the file passed into method. Use markers in the file to determine if there is a question or answer
 		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
 		    String line;
 		    while ((line = br.readLine()) != null) {
--- src/main/Question.java
+++ src/main/Question.java
@@ -5,6 +5,7 @@ public class Question {
 	private String quest;
 	private String answer;
 
+	//check the players answer against the given answer
 	public boolean checkAnswer(String playerAns) {
 		if(playerAns.toLowerCase().equals(answer.toLowerCase())){
 			return true;
@@ -12,6 +13,7 @@ public class Question {
 		return false;
 	}
 	
+	//set the question
 	public void setQuestion(String x) {
 		if(quest == null) {
 			quest = x;
@@ -21,14 +23,17 @@ public class Question {
 		
 	}
 	
+	//get the question
 	public String getQuestion() {
 		return quest;
 	}
 	
+	//set the answer
 	public void setAnswer(String x) {
 		answer = x;
 	}
 	
+	//get the answer
 	public String getAnswer() {
 		return answer;
 	}
